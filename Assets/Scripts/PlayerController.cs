7678827ab7fc316304b782d7a1b4de524a9b9cc1
Watson121 <adamtedders@outlook.com>
2022-07-28 18:47:59 +0100

    Fixed Camera Jitter
    Fixed Camera Jitter Issues
    
    Time.DeltaTime changed to Time.smoothDeltaTime

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,33 +13,31 @@ public class PlayerController : MonoBehaviour
 
     private Transform playerObj;
 
+    [Header("Player Settings")]
     #region Player Inputs
 
     private PlayerInput playerInput;
     private PlayerControls playerControls;
     private CharacterController characterController;
 
-    private float playerSpeed = 10.0f;
+    public float playerSpeed = 10.0f;
     
     private Vector3 playerPosition;
 
     #endregion
 
+    [Header("Camera Settings")]
     #region Camera Settings
 
     public Transform playerCamera;
+    public float mouseSensivity = 30.0f; 
+
     private float CameraRotX;
+    private float CameraRotY;
 
     #endregion
 
-
-    private bool hidden = false;
-
-   
-
- 
-
-    private bool moving = false;
+    public bool hidden = false;
 
     private void Awake()
     {
@@ -58,23 +56,30 @@ public class PlayerController : MonoBehaviour
         Vector2 inputVector = playerControls.Player.Movement.ReadValue<Vector2>();
         Vector2 mouseVector = Mouse.current.delta.ReadValue();
 
+     
+
         // Setting the of player
-        playerObj.rotation *= Quaternion.Euler(0, mouseVector.x * 30.0f * Time.deltaTime, 0);
+        playerObj.rotation *= Quaternion.Euler(0, mouseVector.x * 30.0f * Time.smoothDeltaTime, 0);
+        playerObj.rotation.Normalize();
 
         // Setting the rotation of the camera
         SetCamera();
-        
+
         // Position of character
-        playerPosition = new Vector3(inputVector.x, 0, inputVector.y) * playerSpeed;
+        playerPosition = new Vector3(inputVector.x * playerSpeed, 0, inputVector.y * playerSpeed);
         playerPosition = transform.TransformDirection(playerPosition);
         characterController.Move(playerPosition * Time.deltaTime);
     }
 
     private void SetCamera()
     {
-        CameraRotX += Input.GetAxis("Mouse Y");
+        CameraRotX -= Input.GetAxisRaw("Mouse Y") * mouseSensivity * Time.smoothDeltaTime;
         CameraRotX = Mathf.Clamp(CameraRotX, -40, 40);
-        playerCamera.rotation = Quaternion.Euler(CameraRotX, transform.eulerAngles.y, 0);
+
+        //CameraRotY += Input.GetAxisRaw("Mouse X") * mouseSensivity * Time.deltaTime;
+
+        playerCamera.rotation = Quaternion.Euler(CameraRotX, playerObj.eulerAngles.y, 0);
+
 
         Debug.Log(playerCamera.eulerAngles.x);
     }
@@ -89,5 +94,22 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Movement();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "HiddenZone")
+        {
+            hidden = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "HiddenZone")
+        {
+            hidden = false;
+        }
     }
 }
